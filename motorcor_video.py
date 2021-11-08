6c11580c4736935ef72bd1cac7b40c368eac6a97
nikrus333 <nikrus333@gmail.com>
2021-11-08 16:29:57 +0300

    test line detector made artem

--- motorcor_video.py
+++ motorcor_video.py
@@ -3,24 +3,22 @@
 
 import motorcortex
 import time
-
+import start
 import cv2
 from cv_bridge import CvBridge
 import os
-import rospy
-import tf
 from math import cos, sin, sqrt
 import numpy as np
 
 class tc3():
     def __init__(self):
-        
+        self.test = start.Solution()
         # Creating empty object for parameter tree
         parameter_tree = motorcortex.ParameterTree()
         # Loading protobuf types and hashes
         motorcortex_types = motorcortex.MessageTypes()
         # Open request connection
-        self.ip = "192.168.0.96"
+        self.ip = "192.168.42.1"
         self.frame = "tracking_cam3"
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.req, self.sub = motorcortex.connect("ws://"+self.ip+":5558:5557", motorcortex_types, parameter_tree,
@@ -37,15 +35,18 @@ class tc3():
       
     def onImage(self,val):
         image = cv2.imdecode(np.frombuffer(val[1].value, np.uint8), -1)
+        image_original = image
         #print(type(image))
         path = 'data_base'
 
-        cv2.imwrite(os.path.join(path,'color_black_2' +str(self.count) +'.jpg'), image)
+        #cv2.imwrite(os.path.join(path,'color_black_2' +str(self.count) +'.jpg'), image)
         #self.out.write(image)
-        self.count +=1
-        flag = True
-        if self.count > 70 and flag == True:
-            print('relese')
+        #print(image.shape)
+        image = self.test.binareid(image)
+        image = self.test.transformFunc(image)
+        cv2.imshow('Frame', image)
+        cv2.imshow('Frame_original', image_original)
+        cv2.waitKey(1)
             
             
 
--- start.py
+++ start.py
@@ -1,8 +1,9 @@
 import cv2 as cv
-from matplotlib import image
+
 import numpy as np
 #for test
 import matplotlib.pylab as plt
+from numpy.lib.histograms import histogram
 
 class StartVideo():
     def __init__(self) -> None:
@@ -30,6 +31,8 @@ class Test():
             (width, height),
             (0, height//2)
         ]
+
+    
     def regionOfInterest(self, frame, vertices):
         mask = np.zeros_like(frame)
         #channel_count = frame.shape[2]
@@ -48,25 +51,71 @@ class Test():
         frame = cv.addWeighted(frame, 0.8, line_image, 1, 0.0)
         return frame
 
-        
+class Solution():
+    def __init__(self) -> None:
+        pass
+    def satartData(self, frame):
+        blank_i = 1
+
+    def binareid(self, frame, bin_cof = 220):
+        r_channel = frame[:, :, 2]  
+        binary = np.zeros_like(r_channel)
+        binary[(r_channel > bin_cof)] = 1
+
+        hls = cv.cvtColor(frame, cv.COLOR_BGR2HLS)
+        s_channel = frame[:, :, 2]
+        binary2 = np.zeros_like(s_channel)
+        binary2[(s_channel > 230)] = 1
+
+        all_binary = np.zeros_like(binary)
+        all_binary[((binary == 1) | (binary2 == 1))] = 255
+        return all_binary
+
+    def transformFunc(self, frame):
+        src = np.float32([  [428, 410],
+                            [493, 59],
+                            
+                            [144, 58],
+                            [211, 414]])
+        dot_src = np.int32(src, dtype = np.int32)
+        image_size = [480, 640]                   
+        dst = np.float32([  
+                            [0, 0],
+                            [0, image_size[0]],
+                            [image_size[1], image_size[0]],
+                            [image_size[1], 0]
+                            
+                            
+                                                    ])
+        cv.polylines(frame, [dot_src], True, 200)
+        m_transform = cv.getPerspectiveTransform(src, dst)
+        frame = cv.warpPerspective(frame, m_transform, (image_size[1], image_size[0]), flags=cv.INTER_LINEAR)
+        return frame
+
+    def foundWhiteTable(self, frame):
+        histogram = np.sum(frame[frame.shape[0] // 2 :, :], axis=0)
+
 
 
 if __name__ == "__main__":
     #video = StartVideo()
     #video.main_work()
     test = Test()
-    print(test.frame.shape)
-    gray_image = cv.cvtColor(test.frame, cv.COLOR_RGB2GRAY) 
-    canny_image = cv.Canny(gray_image, 100, 200)
-    croppe_image = test.regionOfInterest(canny_image,
-                    np.array([test.region_interest_view], np.int32),)
+    sol = Solution()
+   # print(test.frame.shape)
+  #  gray_image = cv.cvtColor(test.frame, cv.COLOR_RGB2GRAY) 
+    #canny_image = cv.Canny(gray_image, 100, 200)
+   # croppe_image = test.regionOfInterest(canny_image,
+    #                 np.array([test.region_interest_view], np.int32),)
     
-    lines = cv.HoughLinesP(croppe_image, rho=6, theta=np.pi/60,
-                            threshold=160, lines=np.array([]), 
-                            minLineLength=20, maxLineGap=25)
+    # lines = cv.HoughLinesP(croppe_image, rho=6, theta=np.pi/60,
+    #                         threshold=160, lines=np.array([]), 
+    #                         minLineLength=20, maxLineGap=25)
 
     #image_with_lnes = test.drow_lines(test.frame, lines)
-    plt.imshow(croppe_image)
+    image = sol.binareid(test.frame)
+    image = sol.transformFunc(image)
+    plt.imshow(image)
     
     plt.show()
     #video = StartVideo()
