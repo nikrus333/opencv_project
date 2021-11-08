ccb60b6b3d9008179ff39c8e2069630df55965ff
nikrus333 <nikrus333@gmail.com>
2021-11-08 20:00:45 +0300

    create box show

--- start.py
+++ start.py
@@ -57,7 +57,7 @@ class Solution():
     def satartData(self, frame):
         blank_i = 1
 
-    def binareid(self, frame, bin_cof = 220):
+    def binareid(self, frame, bin_cof = 220): # бинаризация 
         r_channel = frame[:, :, 2]  
         binary = np.zeros_like(r_channel)
         binary[(r_channel > bin_cof)] = 1
@@ -71,10 +71,9 @@ class Solution():
         all_binary[((binary == 1) | (binary2 == 1))] = 255
         return all_binary
 
-    def transformFunc(self, frame):
+    def transformFunc(self, frame):   # аффинные преобразования
         src = np.float32([  [428, 410],
                             [493, 59],
-                            
                             [144, 58],
                             [211, 414]])
         dot_src = np.int32(src, dtype = np.int32)
@@ -84,42 +83,86 @@ class Solution():
                             [0, image_size[0]],
                             [image_size[1], image_size[0]],
                             [image_size[1], 0]
-                            
-                            
                                                     ])
         cv.polylines(frame, [dot_src], True, 200)
         m_transform = cv.getPerspectiveTransform(src, dst)
         frame = cv.warpPerspective(frame, m_transform, (image_size[1], image_size[0]), flags=cv.INTER_LINEAR)
         return frame
 
-    def foundWhiteTable(self, frame):
+    def foundWhiteTable(self, frame):  # поиск беллых линий и отрисовка их 
         histogram = np.sum(frame[frame.shape[0] // 2 :, :], axis=0)
+        midpoint = histogram.shape[0] // 2
+        index_left = np.argmax(histogram[:midpoint])
+        index_right = np.argmax(histogram[midpoint:]) + midpoint
+        warped_visual = frame.copy()
+        cv.line(warped_visual, (index_left, 0), (index_left, warped_visual.shape[0]), 110, 2) # отрисовка линий, тестовое потом удалить 
+        cv.line(warped_visual, (index_right, 0), (index_right, warped_visual.shape[0]), 110, 2)
+        
+        nwindows = 9
+        window_height = np.int(frame.shape[0] / nwindows)
+        window_half_height = 30
+        x_center_left_windows = index_left
+        x_center_right_windows = index_right
+        left_lane = np.array([], dtype=np.int16)
+        right_line = np.array([], dtype=np.int16)
 
+        out_frame = np.dstack((frame, frame, frame)) 
 
+        for window in range(nwindows):
+            win_y1 = frame.shape[0] - (window + 1) * window_height
+            win_y2 = frame.shape[0] - (window) * window_height
+            left_win_x1 = x_center_left_windows - window_half_height
+            left_win_x2 = x_center_left_windows + window_half_height
+            right_win_x1 = x_center_right_windows - window_half_height
+            right_win_x2 = x_center_right_windows + window_half_height
+        
+            cv.rectangle(out_frame, (left_win_x1, win_y1), (left_win_x2, win_y2), (50 + window * 21, 0, 0), 2)
+            cv.rectangle(out_frame, (right_win_x1, win_y1), (right_win_x2, win_y2), (0, 50 + window * 21, 0), 2)
+        return out_frame
+
+    def hough_transform(self, frame):
+    
+        rho = 1              #Distance resolution of the accumulator in pixels.
+        theta = np.pi/180    #Angle resolution of the accumulator in radians.
+        threshold = 20       #Only lines that are greater than threshold will be returned.
+        minLineLength = 20   #Line segments shorter than that are rejected.
+        maxLineGap = 300     #Maximum allowed gap between points on the same line to link them
+        frame =  cv.HoughLinesP(frame, rho = rho, theta = theta, threshold = threshold,
+                            minLineLength = minLineLength, maxLineGap = maxLineGap)
+        return frame
 
 if __name__ == "__main__":
-    #video = StartVideo()
-    #video.main_work()
-    test = Test()
-    sol = Solution()
-   # print(test.frame.shape)
-  #  gray_image = cv.cvtColor(test.frame, cv.COLOR_RGB2GRAY) 
-    #canny_image = cv.Canny(gray_image, 100, 200)
-   # croppe_image = test.regionOfInterest(canny_image,
-    #                 np.array([test.region_interest_view], np.int32),)
-    
-    # lines = cv.HoughLinesP(croppe_image, rho=6, theta=np.pi/60,
-    #                         threshold=160, lines=np.array([]), 
-    #                         minLineLength=20, maxLineGap=25)
 
-    #image_with_lnes = test.drow_lines(test.frame, lines)
-    image = sol.binareid(test.frame)
-    image = sol.transformFunc(image)
-    plt.imshow(image)
+    cap = cv.VideoCapture(0)
+    sol = Solution()
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            original_frame = frame
+            cv.imshow('frame_original',frame)
+            frame_bin = sol.binareid(frame)
+            ##frame_transofrm = sol.transformFunc(frame_bin)
+            #print(type(frame_bin))
+            frame_hough = sol.hough_transform(frame_bin)
+            frame_white_table =  sol.foundWhiteTable(frame_bin)
+            cv.imshow('frame_bin',frame_bin)
+            cv.imshow('frame_white_table',frame_white_table)
+            #cv.imshow('frame_trans',frame_transofrm)
+            #cv.imshow('frame_trans',frame_hough)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        else:
+            break
     
-    plt.show()
-    #video = StartVideo()
-    #video.main_work()
+    cap.release()
+
+    cv.destroyAllWindows()
+
+# Release everything if job is finished
+
+
 
    
 
