f84abf2272cfa4d922eb56d7bbe0acaaecce1117
nikrus333 <nikrus333@gmail.com>
2021-11-30 13:05:08 +0300

    only blob for delta

--- motorcor_video.py
+++ motorcor_video.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
-
 import motorcortex
-import time
-import start
-import cv2
-from cv_bridge import CvBridge
+import cv2 as cv
+import mcx_tracking_cam_pb2 as tracking_cam_msg
 import os
 from math import cos, sin, sqrt
 import numpy as np
 
 class tc3():
     def __init__(self):
-        self.test = start.Solution()
+        self.data = None
+
         # Creating empty object for parameter tree
         parameter_tree = motorcortex.ParameterTree()
         # Loading protobuf types and hashes
@@ -25,37 +23,37 @@ class tc3():
                                     certificate=dir_path+"/motorcortex.crt", timeout_ms=1000,
                                     login="root", password="vectioneer")
 
-        self.bridge = CvBridge()
-        
         self.subscription6 = self.sub.subscribe(["root/Comm_task/utilization_max","root/Processing/image"], "camera", 1)
         self.subscription6.get()
         self.subscription6.notify(self.onImage)
         #self.out = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 1,(640,480))
-        self.count = 1
-      
+        
+        self.subscription2 = self.sub.subscribe(["root/Processing/BlobDetector/blobBuffer"], "blob", 1)
+        self.subscription2.get()
+        self.subscription2.notify(self.onBlob)
+        self.BlobsBlobs = tracking_cam_msg.Blobs
+
+    
+    def onBlob(self,val):
+        print("find blob")
+        try:
+            blobs = tracking_cam_msg.Blobs()
+            if blobs.ParseFromString(val[0].value):
+                print(blobs.value)
+                self.data = blobs.value
+        except Exception as e:
+            print(e)
+
     def onImage(self,val):
-        image = cv2.imdecode(np.frombuffer(val[1].value, np.uint8), -1)
-        image_original = image
-        #print(type(image))
-        path = 'data_base'
+        frame = cv.imdecode(np.frombuffer(val[1].value, np.uint8), -1)
+        image_original = frame
+        cv.waitKey(1)
 
-        #cv2.imwrite(os.path.join(path,'color_black_2' +str(self.count) +'.jpg'), image)
-        #self.out.write(image)
-        #print(image.shape)
-        image = self.test.binareid(image)
-        image = self.test.transformFunc(image)
-        cv2.imshow('Frame', image)
-        cv2.imshow('Frame_original', image_original)
-        cv2.waitKey(1)
-            
-            
-
-        #cv2.imshow('Frame', image)
 if __name__ == '__main__':
     tc3_ex = tc3()
     while True:
         try:
-           t = 0
+           print(tc3_ex.data)
         except Exception as e:
             print(e)
             tc3_ex.req.close()
